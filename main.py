ed273c2a0973e8756ebf30b841974e9faf2589ca
Rafael Nacle <rafaelnacle@hotmail.com>
2024-01-22 18:13:29 -0300

    added count_letters function

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     path = "./books/frankenstein.txt"
     text = get_book_text(path)
     word_count = count_words(text)
-    print(word_count)
+    letter_count = count_letters(text)
+    print(letter_count)
 
 
 def get_book_text(path):
@@ -13,6 +14,20 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+
+def count_letters(text):
+    lower_text = text.lower()
+    letter_dict = {}
+
+    for letter in lower_text:
+        if letter.isalpha():
+            if letter in letter_dict:
+                letter_dict[letter] += 1
+            else:
+                letter_dict[letter] = 1
+
+    return letter_dict
 
 
 main()
