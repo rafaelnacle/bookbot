16e2a0ed0988151b0201d8bd71f4780239f46231
Rafael Nacle <rafaelnacle@hotmail.com>
2024-01-22 18:03:54 -0300

    count words function

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def main():
-  path = "./books/frankenstein.txt"
-  text = get_book_text(path)
-  print(text)
+    path = "./books/frankenstein.txt"
+    text = get_book_text(path)
+    word_count = count_words(text)
+    print(word_count)
+
 
 def get_book_text(path):
-  with open("./books/frankenstein.txt", encoding="utf-8") as file:
-    return file.read()
+    with open("./books/frankenstein.txt", encoding="utf-8") as file:
+        return file.read()
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
 
 main()
